1d4b1652973bfabc2bf47a3b959b4e05e3027fa1
Daniel Machado <daniellgmachado@gmail.com>
2022-04-23 20:07:51 +1000

    first

--- initgit.cmd
+++ initgit.cmd
@@ -2,5 +2,5 @@ echo "# danfestajuninadotnet6" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
-	git remote add origin git@github.com:DR2010/danfestajuninadotnet6.git
+git remote add origin git@github.com:DR2010/festajuninawebdn.git
 git push -u origin master
